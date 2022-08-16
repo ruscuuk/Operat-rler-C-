0e48b73d6c5b2b39c716ee1fa5149c6e88988627
ruscuuk <you@example.com>
2022-08-16 17:09:04 +0300

    Try Catch Denemesi

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+try
+{
+    int a = int.Parse(Console.ReadLine());
+
+    int b = int.Parse(Console.ReadLine());
+
+    int c = a+b;
+
+    Console.WriteLine(c);
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Bir Hata Oluştu: "+ ex.Message);
+}
+finally
+{
+    Console.WriteLine("İşlem tamamlandı.");
+}
